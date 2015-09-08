fb34c148dde4c05848f15c7cbc351a4d1e64a63a
WYSIWIG_AX <adigo.axel@gmail.com>
2015-09-09 00:44:16 +0200

    install fosUserBundle and create userBundle

---
+++ src/Sms/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sms\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('SmsUserBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Sms/UserBundle/SmsUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sms\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SmsUserBundle extends Bundle
+{
+}
